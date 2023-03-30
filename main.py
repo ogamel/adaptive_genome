006ca2e0b0671fb8d3e9711f7ce364b889ee0425
Omar <omar@pacific.local>
2023-03-30 18:12:37 -0400

    main.py modeling section

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ or annotation information.
 Conservation score is measured by Genomic Evolutionary Rate Profiling (GERP).
 All data obtained from Ensembl release 109
 """
+import logging
 
 from data.load import read_sequence, read_annotation_generator, read_gerp_scorer
 from data.paths import chr17_paths  # paths to source data files
 from score_analysis import score_stats_by_kmer
+from score_modeling import get_train_test_x_y, LocalWindowModel, ModelTrainer
 
 if __name__ == '__main__':
     # start the analysis with human chromosome 17
@@ -25,7 +27,17 @@ if __name__ == '__main__':
     # get GERP retrieval function, from the BigWig file
     gerp_scorer = read_gerp_scorer(paths.gerp)
 
+    """Analysis"""
     # analyze by kmer for CDS (coding sequence) features
     kmer_base_df = score_stats_by_kmer(seq_records_gen, gerp_scorer, ['CDS'], k_values=[1, 2, 3])
 
+    """Modeling (neural network in JAX)"""
+    x_train, y_train, x_test, y_test = get_train_test_x_y(seq_records_gen, gerp_scorer, ['CDS'])
 
+    model = LocalWindowModel()
+
+    model_trainer = ModelTrainer(model, epochs=1000)
+    model_trainer.train(x_train, y_train)
+
+    loss_test = model_trainer.model.loss(x_test, y_test)
+    logging.info(f'Test loss {loss_test:.3f}.')
