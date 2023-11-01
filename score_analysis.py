eceee9480fde432c0e4003a6e108a97005fcbc6a
Omar <omar.gamel@utoronto.ca>
2023-10-31 23:52:48 -0700

    stub for p value comparison instead of correlation

--- score_analysis.py
+++ score_analysis.py
@@ -31,7 +31,7 @@ def corrcoefs_by_score_count(df_in: pd.DataFrame, kmer_col: str = KMER_COL, coun
     sortby_cols = [col for col in df_in.columns if col in ID_COLS]
     df = df_in.sort_values(by=sortby_cols)
 
-    # list to facilitate inverse position sorting in the reverse complement dataframe,
+    # list to facilitate inverse position and strand sorting in the reverse complement dataframe,
     # e.g. to measure correlation first position in kmer with last in kmer_rc, etc.
     rc_sort_ascending = [False if col in [POS_COL, STRAND_COL] else True for col in sortby_cols]
 
@@ -78,6 +78,82 @@ def corrcoefs_by_score_count(df_in: pd.DataFrame, kmer_col: str = KMER_COL, coun
             # invert position sorting in reverse complement, if position column exists,
             df_kmer_rc = df_kmer_rc.sort_values(by=sortby_cols, ascending=rc_sort_ascending)
 
+            rc_pairwise_counts[0].extend(df_kmer[count_col])
+            rc_pairwise_counts[1].extend(df_kmer_rc[count_col])
+            rc_pairwise_scores[0].extend(df_kmer[score_col])
+            rc_pairwise_scores[1].extend(df_kmer_rc[score_col])
+
+        corr_pairwise_count = np.corrcoef(*rc_pairwise_counts)[0][1]
+        print(f'k = {k}, pairwise reverse complement correlation between k-mer counts {corr_pairwise_count:0.3f}')
+
+        corr_pairwise_score = np.corrcoef(*rc_pairwise_scores)[0][1]
+        print(f'k = {k}, pairwise reverse complement correlation between k-mer scores {corr_pairwise_score:0.3f}')
+
+    return  # corr_count_score, corr_pairwise_count, corr_pairwise_score
+
+
+def pvalues_by_score_count(df_in: pd.DataFrame, kmer_col: str = KMER_COL, count_col: str = COUNT_COL,
+                             score_col: str = SCORE_MEAN_COL, k_values: list[int] = None):
+    """
+    Find pvalues of score and counts of reverse complement kmers being equal, stratified by the ID_FIELDS present in
+    the dataframe and their hardcoded relationships that we test.
+    Input df is output from score_stats_by_kmer, possibly subsequently further aggregated.
+    """
+
+    if k_values is None:
+        k_values = df_in[K_COL].unique()
+
+    sortby_cols = [col for col in df_in.columns if col in ID_COLS]
+    df = df_in.sort_values(by=sortby_cols)
+
+    # list to facilitate inverse position sorting in the reverse complement dataframe,
+    # e.g. to measure correlation first position in kmer with last in kmer_rc, etc.
+    rc_sort_ascending = [False if col in [POS_COL, STRAND_COL] else True for col in sortby_cols]
+
+    for k in k_values:
+        df_k = df[df[K_COL] == k]
+
+        # start with simplest correlation
+        corr_count_score = np.corrcoef(df_k[count_col], df_k[score_col])[0][1]
+        print(f'k = {k}, correlation between k-mer count and score {corr_count_score:0.3f}')
+
+        # initiate lists for pairwise values, for a kmer and its reverse complement
+        rc_pairwise_counts = [[], []]
+        rc_pairwise_scores = [[], []]
+
+        kmers_set = set(kmers_in_rc_order(k))
+        while kmers_set:
+            kmer = kmers_set.pop()
+            kmer_rc = reverse_complement(kmer)
+
+            # skip palindromes, as correlating the same quantity with itself will artificially raise correlation
+            # TODO: there should be an exception for scores (but not counts) if POS_COL in sortby_cols, since measuring
+            #  score correlation between different positions of a palindrome is valid, but perhaps only half of each
+            #  palindrome to avoid duplication?
+            #  Similarly, if k is odd, consider excluding the middle position since then we are measuring correlation
+            #  with itself.
+            if kmer == kmer_rc:
+                continue
+            kmers_set.remove(kmer_rc)
+
+            if kmer > kmer_rc:  # alphabetically order kmer and its reverse complement
+                kmer, kmer_rc = kmer_rc, kmer
+
+            df_kmer = df_k[df_k[KMER_COL] == kmer]
+            df_kmer_rc = df_k[df_k[KMER_COL] == kmer_rc]
+
+            # TODO: generalize this to k>3? Understand why this observation holds.
+            # switch frames 0 and 1 in the reverse complement for correlation calculation purposes
+            def frame_map(x):
+                return (-k - x) % 3
+
+            if FRAME_COL in df_kmer.columns:
+                df_kmer_rc.loc[:, FRAME_COL] = df_kmer_rc[FRAME_COL].map(frame_map)
+
+            # invert position sorting in reverse complement, if position column exists,
+            df_kmer_rc = df_kmer_rc.sort_values(by=sortby_cols, ascending=rc_sort_ascending)
+
+            # TODO: replace correlation with p values
             rc_pairwise_counts[0].extend(df_kmer[count_col])
             rc_pairwise_counts[1].extend(df_kmer_rc[count_col])
             rc_pairwise_scores[0].extend(df_kmer[score_col])
